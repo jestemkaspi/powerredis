7d90007a6f447669882897c075434404941281ff
Piotr Kasperski <kspp@viessmann.com>
2017-06-26 11:13:48 +0200

    PRTG command

--- PowerRedis2/Server.cs
+++ PowerRedis2/Server.cs
@@ -2,8 +2,9 @@
 using ServiceStack.Redis;
 using System.Collections.Generic;
 using System;
-
-
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using System.Collections;
 
 namespace PowerRedis2
 {
@@ -23,7 +24,52 @@ namespace PowerRedis2
         protected override void ProcessRecord()
         {
             Dictionary<string, string> info = Globals.rc.Info;
-            WriteObject(info);
+
+            string json = @"{{
+  ""prtg"": {{
+    ""result"": [
+      {{
+        ""channel"": ""Connected Clients"",
+        ""value"": ""{0}"",
+        ""unit"": ""Count""
+      }},
+      {{
+        ""channel"": ""Blocked Clients"",
+        ""value"": ""{1}"",
+        ""unit"": ""Count""
+      }},
+      {{
+        ""channel"": ""Used Memory"",
+        ""value"": ""{2}"",
+        ""unit"": ""BytesMemory""
+      }},
+      {{
+        ""channel"": ""Total Connections Received"",
+        ""value"": ""{3}"",
+        ""unit"": ""Count""
+      }},
+      {{
+        ""channel"": ""Instantaneous Operations per Second"",
+        ""value"": ""{4}"",
+        ""unit"": ""Count""
+      }},
+      {{
+        ""channel"": ""Expired Keys"",
+        ""value"": ""{5}"",
+        ""unit"": ""Count""
+      }}
+    ]
+  }}
+}}";
+            string result = string.Format(json, info["connected_clients"]
+  , info["blocked_clients"]
+  , info["used_memory"]
+  , info["total_connections_received"]
+  , info["instantaneous_ops_per_sec"]
+  , info["expired_keys"]);
+
+
+            WriteObject(result);
         }
 
         protected override void EndProcessing()
